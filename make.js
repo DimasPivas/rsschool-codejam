db72b7a37f17796b05a5b146dd669dbe8d4cbd48
Dmitriy <dmitriy_metelskiy@mail.ru>
2018-10-28 20:15:08 +0300

    Add solution in make.js through closure

--- make.js
+++ make.js
@@ -1,7 +1,18 @@
-function sum(a, b) {
-    return a + b;
+// Через замыкание
+function make(...arg0) {
+    let argArray = [];
+    let func = (...arr) => {
+        if (typeof arr[0] === 'function') {
+            return argArray.reduce(arr[0]);
+        }
+        else {
+    	    argArray.push(...arr);
+	        return func;
+        }
+    };
+    return func(...arg0);
 }
-
+/*
 function make(...arr) {
     let len = arr.length;
     if (typeof arr[len - 1] === 'function') {
@@ -11,5 +22,8 @@ function make(...arr) {
 	    return make.bind(null, ...arr);
     }
 }
-
+*/
+function sum(a, b) {
+    return a + b;
+}
 console.log(make(15)(34,21,666)(41)(sum));
