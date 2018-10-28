98a1412c6d3d14ea580fec98bf04b6a4f0f90c7e
Dmitriy <dmitriy_metelskiy@mail.ru>
2018-10-28 20:44:47 +0300

    Add some tests in make.js

--- make.js
+++ make.js
@@ -11,4 +11,7 @@ function make(...arr) {
 function sum(a, b) {
     return a + b;
 }
-console.log(make(15)(34,21,666)(41)(sum));
+console.log(make(15)(34,21,666)(41)(sum));  // 777
+console.log(make(1, 2)(3, 4, 5)(6)(sum));   // 21
+console.log(make(1)(sum));                  // 1
+console.log(make(5, 15.5)(sum));            // 20.5
