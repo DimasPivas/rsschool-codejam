4de1fd007c40746827377bff36676e1443758d9f
Dmitriy <dmitriy_metelskiy@mail.ru>
2018-10-28 20:50:54 +0300

    Add some tests

--- recursion.js
+++ recursion.js
@@ -2,7 +2,12 @@ let tree = {"value":100,
     "left": {"value":90, "left":{"value":70}, "right":{"value":99 }},
     "right":{"value":120,"left":{"value":110},"right":{"value":130}}
 };
-// depth - индекс массива, в который нужно запушить
+let tree2 = {"value":1,
+"left": {"value":2, "right":{"value":3}},
+"right":{"value":4,"left":{"value":5}}
+};
+let tree3 = {"value":125};
+
 function recursion(currentNode, depth = 0, res = []) {
     if(!currentNode) {
         return res;
@@ -18,3 +23,5 @@ function recursion(currentNode, depth = 0, res = []) {
 
 let array = recursion(tree);
 console.log(array); // [[100], [90, 120], [70,99,110,130]]
+console.log(recursion(tree2));  // [[1], [2, 4], [3, 5]]
+console.log(recursion(tree3));  // [[125]]
